85b05f763f4350a0129964fd8e0afaa08c053e47
Juan Manuel España <guadalinfo.almachar@guadalinfo.es>
2022-06-28 18:47:50 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace T03_Eje5
+{
+    class T03_Eje5
+    {
+        static void Main()
+        {
+            int A = 1;
+            int B = 2;
+            int C = 3;  
+            int D = 4;
+            int Aux;
+
+            Console.WriteLine("Valores iniciales: A={0}, B={1}, C={2} y D={3}", A, B, C, D);
+
+            Aux = A;
+            A = D;
+            D = B;
+            B = C;
+            C = Aux;
+
+            Console.WriteLine("Valores finales: A={0}, B={1}, C={2} y D={3}", A, B, C, D);
+
+            Console.WriteLine("Pulse cualquier tecla para continuar...");
+            Console.ReadKey();
+        }
+    }
+}
